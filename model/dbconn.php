7d696eab0cdbec46aaf5607d3fd53d9d76bb2b9d
zzunebye <bhang96@gmail.com>
2021-05-02 18:16:49 +0800

    commit

--- model/dbconn.php
+++ model/dbconn.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "localhost";
-$database  = "waddproject";
-$username = "root";
-$password  = "";
+$hostname = "us-cdbr-east-03.cleardb.com";
+$database  = "heroku_60778271f37c57f";
+$username = "b76be395b2bf68";
+$password  = "b695d003";
 $conn = mysqli_connect($hostname, $username, $password, $database)
 	or die(mysqli_connect_error());
 
